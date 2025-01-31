84fdfe19a0ed64ad6aa3675d7bf5cf24f183a3ba
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2025-01-31 23:23:58 +0530

    Add Prisma integration with user synchronization and database schema

---
+++ src/actions/user.action.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) return;
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+    if (existingUser) return existingUser;
+
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+    return newUser;
+  } catch (error) {
+    console.error("Error in syncUser", error);
+  }
+}
+
+export async function getUserByClerkId(clerkId: string) {
+  return await prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true,
+        },
+      },
+    },
+  });
+}
